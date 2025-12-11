3fd0fbf4fe27e0c3c586731aff4b9f24563cd74c
Ali Raza <aliraza93644@gmail.com>
2025-12-11 12:04:06 +0500

    update next js and fix deployment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 import { auth } from '@/auth'
+import EditRoleMobile from '@/components/EditRoleMobile'
 import dbConnect from '@/config/db'
+import User from '@/models/user.model'
+import { redirect } from 'next/navigation'
 import React from 'react'
+
 
 const Home = async () => {
   await dbConnect()
   let session  = await auth()
-  console.log(session)
+  const user =  await User.findById(session?.user?.id)
+  console.log(user)
+  if(!user){
+   redirect('/login')
+  }
+  const inCompleteProfile = !user?.mobile || !user?.role || (!user.mobile && user.role === 'user')
+  if(inCompleteProfile){
+    return <EditRoleMobile/>
+  }
+
   return (
     <div>
       
