a5984770cfe6de2ade5f7d4e9b9edd1733b1e7e4
Ali Raza <aliraza93644@gmail.com>
2025-12-11 20:36:24 +0500

    search ui

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -24,10 +24,14 @@ const [open,setOpen] = useState(false)
             Grocify
       </Link>
 
-      <form className='hidden md:flex items-center bg-white rounded-full px-4 py-3 w-1/2 max-w-lg'>
-      <Search className='text-gray-500 w-5 h-5 mr-2'/>
-      <input type="text" placeholder='Search groceries...' className='w-full outline-none text-gray-700 placeholder-gray-400'/>
-      </form>
+      <form className='hidden md:flex items-center bg-white rounded-full px-4 py-2 w-1/2 max-w-lg min-h-10'>
+  <Search className='text-gray-500 w-5 h-5 mr-2'/>
+  <input
+    type="text"
+    placeholder='Search groceries...'
+    className='flex-1 outline-none text-gray-700 placeholder-gray-400 bg-transparent'
+  />
+</form>
 
       <div className='flex items-center gap-3 md:gap-6 relative '>
     <Link href={''} className='relative bg-white rounded-full w-11 h-11 flex items-center justify-center shadow-md hover:scale-105 transition '>
