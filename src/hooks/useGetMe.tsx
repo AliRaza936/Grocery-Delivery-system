d57c43ffd40bb404e073fc2959dccc1e9845ed71
Ali Raza <aliraza93644@gmail.com>
2025-12-16 20:34:44 +0500

    redux tool kit install

--- src/hooks/useGetMe.tsx
+++ src/hooks/useGetMe.tsx
@@ -2,7 +2,8 @@
 import { AppDispatch } from '@/redux/store'
 import { setUserData } from '@/redux/userSlice'
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
+
 import {useDispatch} from 'react-redux'
 
 function useGetMe() {
