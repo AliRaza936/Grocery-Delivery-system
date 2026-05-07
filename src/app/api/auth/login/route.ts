555ccedbcb78f3ab0d56b3968fc6fe7035aabb1f
Ali Raza <aliraza93644@gmail.com>
2026-05-07 18:43:37 +0500

    change token expiry

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -43,7 +43,7 @@ export async function POST(req: NextRequest) {
         role: user.role,
       },
       process.env.AUTH_SECRET!,
-      { expiresIn: "7d" }
+      
     );
 
     // 4. Return response
