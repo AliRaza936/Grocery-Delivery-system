8a5ed55fb48c46264e33e22d4305a897a87b758d
Ali Raza <aliraza93644@gmail.com>
2026-05-07 18:33:00 +0500

    login api

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,63 @@
+import dbConnect from "@/config/db";
+import User from "@/models/user.model";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+export async function POST(req: NextRequest) {
+  try {
+    await dbConnect();
+
+    const { email, password } = await req.json();
+
+    // 1. Check user exists
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json(
+        { success: false, message: "Invalid email or password" },
+        { status: 400 }
+      );
+    }
+
+    // 2. Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return NextResponse.json(
+        { success: false, message: "Invalid email or password" },
+        { status: 400 }
+      );
+    }
+
+    // 3. Create JWT token
+    const token = jwt.sign(
+      {
+        id: user._id,
+        email: user.email,
+        role: user.role,
+      },
+      process.env.JWT_SECRET!,
+      { expiresIn: "7d" }
+    );
+
+    // 4. Return response
+    return NextResponse.json({
+      success: true,
+      message: "Login successful",
+      token,
+      user: {
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    });
+
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: `login error ${error}` },
+      { status: 500 }
+    );
+  }
+}
