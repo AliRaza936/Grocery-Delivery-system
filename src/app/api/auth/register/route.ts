0c8729a7fed21749012352b884b04c6ec16a0e2e
Ali Raza <aliraza93644@gmail.com>
2026-05-10 15:47:38 +0500

    mobile register api

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,28 +1,66 @@
 import dbConnect from "@/config/db";
 import User from "@/models/user.model";
 import { NextRequest, NextResponse } from "next/server";
-import bcrypt from "bcryptjs"
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-export async function POST(req:NextRequest){
-    try {
-    await dbConnect()
-    const {name,email,password} = await req.json()
+export async function POST(req: NextRequest) {
+  try {
+    await dbConnect();
 
-    let userExist = await User.findOne({email})
-    if(userExist){
-        return NextResponse.json({success:false,message:"email already exist"},{status:400})
+    const { name, email, password } = await req.json();
+
+    // check user exists
+    let userExist = await User.findOne({ email });
+    if (userExist) {
+      return NextResponse.json(
+        { success: false, message: "email already exist" },
+        { status: 400 }
+      );
     }
-    if(password.length < 6){
-        return NextResponse.json({success:false,message:"password must be at least 6 characters"},{status:400})
-    }
-    let hashedPassword = await bcrypt.hash(password,10)
-    let user = await User.create({name,email,password:hashedPassword})
 
-        return NextResponse.json({success:true,message:"user created successfully",user},{status:200})
-
+    // password validation
+    if (password.length < 6) {
+      return NextResponse.json(
+        { success: false, message: "password must be at least 6 characters" },
+        { status: 400 }
+      );
     }
-catch (error) {
-        return NextResponse.json({success:false,message:`register error ${error}`},{status:500})
-    
-}
+
+    // hash password
+    let hashedPassword = await bcrypt.hash(password, 10);
+
+    // create user
+    let user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    // 🔐 CREATE TOKEN
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.AUTH_SECRET as string,
+      
+    );
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "user created successfully",
+        token,
+        user: {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+        },
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { success: false, message: `register error ${error}` },
+      { status: 500 }
+    );
+  }
 }
