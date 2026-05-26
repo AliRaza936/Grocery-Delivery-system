99c14175f3c34bf974bb4ac722bdd69932ffc149
Ali Raza <aliraza93644@gmail.com>
2026-05-26 17:38:19 +0500

    load schema

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,32 +1,37 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
-let mongodbUrl = process.env.MONGODB_URL 
+let mongodbUrl = process.env.MONGODB_URL;
 
-if(!mongodbUrl){
-    throw new Error("db error")
+if (!mongodbUrl) {
+  throw new Error("db error");
 }
 
-let cached = global.mongoose
+let cached = global.mongoose;
 
-
-if(!cached){
-    cached = global.mongoose = {conn:null,promise:null}
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
-let dbConnect = async ()=>{
-   if(cached.conn){
-    return cached.conn
-   }
-   if(!cached.promise){
-    cached.promise = mongoose.connect(mongodbUrl).then((conn)=>conn.connection)
-   }
-   try {
-    let conn = await cached.promise
-    
-    return conn
-   } catch (error) {
-    console.log(error)
-   }
-}
-export default dbConnect
+let dbConnect = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
 
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(mongodbUrl).then((conn) => conn.connection);
+
+    // 🔥 FORCE LOAD MODELS HERE
+    await import("@/models/order.model");
+    await import("@/models/deliveryAssignment.model");
+  }
+
+  try {
+    let conn = await cached.promise;
+    cached.conn = conn;
+    return conn;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default dbConnect;
